symfony/symfony

Implement service-based Resource (cache) validation
Currently, any metadata passed to ConfigCache (namely implementations of ResourceInterface) is serialized to disk. When the ConfigCache is validated, the metadata is unserialized and queried through ResourceInterface::isFresh() to determine whether the caceh is fresh. That way, ResourceInterface implementations cannot interact with services, for example a database connection.

This PR introduces the new concept of ResourceCheckers. Services implementing ResourceCheckerInterface can be tagged as "config_cache.resource_checker" with an optional priority.

Clients that wish to use ConfigCache can then obtain an instance from the config_cache_factory service (which implements ConfigCacheFactoryInterface). The factory will take care of injecting resource checkers into the ConfigCache instance so that they can be used for cache validation.

Checking cache metadata is easy for ResourceCheckers:
- First, the ResourceCheckerInterface::supports() implementation is passed the metadata object in question. If the checker cannot handle the type of resource passed, support() should return false.
- Otherwise, the ResourceCheckerInterface::isFresh() method will be called and given the resource as well as the timestamp at which the cache was initialized. If that method returns false, the cache is considered stale. If it returns true, the resource is considered valid and will *not* be passed to any additional checkers.

BC and migration path:

This PR does not (intend to) break BC but it comes with deprecations. The main reason is that ResourceInterface contains an isFresh() method that does not make sense in the general case of resources.

Thus, ResourceInterface::isFresh() is marked as deprecated and shall be removed in Symfony 3.0. Resource implementations that can (or wish to) be validated in that simple manner can implement the SelfCheckingResourceInterface sub-interface that still contains (and will keep) the isFresh() method. 

src/Symfony/Component/Config/ConfigCache.php
@@ -11,22 +11,27 @@
 
 namespace Symfony\Component\Config;
 
-use Symfony\Component\Config\Resource\ResourceInterface;
-use Symfony\Component\Filesystem\Exception\IOException;
-use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Config\Resource\BCResourceInterfaceChecker;
+use Symfony\Component\Config\Resource\DefaultResourceChecker;
 
 /**
- * ConfigCache manages PHP cache files.
+ * ConfigCache caches arbitrary content in files on disk.
  *
- * When debug is enabled, it knows when to flush the cache
- * thanks to an array of ResourceInterface instances.
+ * When in debug mode, those metadata resources that implement
+ * \Symfony\Component\Config\Resource\SelfCheckingResourceInterface will
+ * be used to check cache freshness.
+ *
+ * During a transition period, also instances of
+ * \Symfony\Component\Config\Resource\ResourceInterface will be checked
+ * by means of the isFresh() method. This behaviour is deprecated since 2.8
+ * and will be removed in 3.0.
  *
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author Matthias Pigulla <mp@webfactory.de>
  */
-class ConfigCache implements ConfigCacheInterface
+class ConfigCache extends ResourceCheckerConfigCache
 {
     private $debug;
-    private $file;
 
     /**
      * @param string $file  The absolute cache path
@@ -34,7 +39,10 @@ class ConfigCache implements ConfigCacheInterface
      */
     public function __construct($file, $debug)
     {
-        $this->file = $file;
+        parent::__construct($file, array(
+            new DefaultResourceChecker(),
+            new BCResourceInterfaceChecker(),
+        ));
         $this->debug = (bool) $debug;
     }
 
@@ -49,90 +57,23 @@ class ConfigCache implements ConfigCacheInterface
     {
         @trigger_error('ConfigCache::__toString() is deprecated since version 2.7 and will be removed in 3.0. Use the getPath() method instead.', E_USER_DEPRECATED);
 
-        return $this->file;
-    }
-
-    /**
-     * Gets the cache file path.
-     *
-     * @return string The cache file path
-     */
-    public function getPath()
-    {
-        return $this->file;
+        return $this->getPath();
     }
 
     /**
      * Checks if the cache is still fresh.
      *
-     * This method always returns true when debug is off and the
+     * This implementation always returns true when debug is off and the
      * cache file exists.
      *
      * @return bool true if the cache is fresh, false otherwise
      */
     public function isFresh()
     {
-        if (!is_file($this->file)) {
-            return false;
-        }
-
-        if (!$this->debug) {
+        if (!$this->debug && is_file($this->getPath())) {
             return true;
         }
 
-        $metadata = $this->getMetaFile();
-        if (!is_file($metadata)) {
-            return false;
-        }
-
-        $time = filemtime($this->file);
-        $meta = unserialize(file_get_contents($metadata));
-        foreach ($meta as $resource) {
-            if (!$resource->isFresh($time)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Writes cache.
-     *
-     * @param string              $content  The content to write in the cache
-     * @param ResourceInterface[] $metadata An array of ResourceInterface instances
-     *
-     * @throws \RuntimeException When cache file can't be written
-     */
-    public function write($content, array $metadata = null)
-    {
-        $mode = 0666;
-        $umask = umask();
-        $filesystem = new Filesystem();
-        $filesystem->dumpFile($this->file, $content, null);
-        try {
-            $filesystem->chmod($this->file, $mode, $umask);
-        } catch (IOException $e) {
-            // discard chmod failure (some filesystem may not support it)
-        }
-
-        if (null !== $metadata && true === $this->debug) {
-            $filesystem->dumpFile($this->getMetaFile(), serialize($metadata), null);
-            try {
-                $filesystem->chmod($this->getMetaFile(), $mode, $umask);
-            } catch (IOException $e) {
-                // discard chmod failure (some filesystem may not support it)
-            }
-        }
-    }
-
-    /**
-     * Gets the meta file path.
-     *
-     * @return string The meta file path
-     */
-    private function getMetaFile()
-    {
-        return $this->file.'.meta';
+        return parent::isFresh();
     }
 }

src/Symfony/Component/Config/Resource/BCResourceInterfaceChecker.php
@@ -1,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Config\Resource;
+
+/**
+ * Resource checker for the ResourceInterface. Exists for BC.
+ *
+ * @author Matthias Pigulla <mp@webfactory.de>
+ *
+ * @deprecated since 2.8, to be removed in 3.0.
+ */
+class BCResourceInterfaceChecker extends DefaultResourceChecker
+{
+    public function supports(ResourceInterface $metadata)
+    {
+        /* As all resources must be instanceof ResourceInterface,
+           we support them all. */
+        return true;
+    }
+
+    public function isFresh(ResourceInterface $metadata, $timestamp)
+    {
+        trigger_error('Resource checking through ResourceInterface::isFresh() is deprecated since 2.8 and will be removed in 3.0', E_USER_DEPRECATED);
+
+        return parent::isFresh($metadata, $timestamp); // For now, $metadata features the isFresh() method, so off we go (quack quack)
+    }
+}

src/Symfony/Component/Config/ResourceCheckerInterface.php
@@ -1,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Config;
+
+use Symfony\Component\Config\Resource\ResourceInterface;
+
+/**
+ * Interface for ResourceCheckers.
+ *
+ * When a ResourceCheckerConfigCache instance is checked for freshness, all its associated
+ * metadata resources are passed to ResourceCheckers. The ResourceCheckers
+ * can then inspect the resources and decide whether the cache can be considered
+ * fresh or not.
+ *
+ * @author Matthias Pigulla <mp@webfactory.de>
+ * @author Benjamin Klotz <bk@webfactory.de>
+ */
+interface ResourceCheckerInterface
+{
+    /**
+     * Queries the ResourceChecker whether it can validate a given
+     * resource or not.
+     *
+     * @param ResourceInterface $metadata The resource to be checked for freshness
+     *
+     * @return bool True if the ResourceChecker can handle this resource type, false if not
+     */
+    public function supports(ResourceInterface $metadata);
+
+    /**
+     * Validates the resource.
+     *
+     * @param object $metadata  The resource to be validated.
+     * @param int    $timestamp The timestamp at which the cache associated with this resource was created.
+     *
+     * @return bool True if the resource has not changed since the given timestamp, false otherwise.
+     */
+    public function isFresh(ResourceInterface $metadata, $timestamp);
+
+}

chasepeeler/config

ConfigCache.php
@@ -11,22 +11,27 @@
 
 namespace Symfony\Component\Config;
 
-use Symfony\Component\Config\Resource\ResourceInterface;
-use Symfony\Component\Filesystem\Exception\IOException;
-use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Config\Resource\BCResourceInterfaceChecker;
+use Symfony\Component\Config\Resource\SelfCheckingResourceChecker;
 
 /**
- * ConfigCache manages PHP cache files.
+ * ConfigCache caches arbitrary content in files on disk.
  *
- * When debug is enabled, it knows when to flush the cache
- * thanks to an array of ResourceInterface instances.
+ * When in debug mode, those metadata resources that implement
+ * \Symfony\Component\Config\Resource\SelfCheckingResourceInterface will
+ * be used to check cache freshness.
+ *
+ * During a transition period, also instances of
+ * \Symfony\Component\Config\Resource\ResourceInterface will be checked
+ * by means of the isFresh() method. This behaviour is deprecated since 2.8
+ * and will be removed in 3.0.
  *
  * @author Fabien Potencier <fabien@symfony.com>
+ * @author Matthias Pigulla <mp@webfactory.de>
  */
-class ConfigCache implements ConfigCacheInterface
+class ConfigCache extends ResourceCheckerConfigCache
 {
     private $debug;
-    private $file;
 
     /**
      * @param string $file  The absolute cache path
@@ -34,7 +39,10 @@ class ConfigCache implements ConfigCacheInterface
      */
     public function __construct($file, $debug)
     {
-        $this->file = $file;
+        parent::__construct($file, array(
+            new SelfCheckingResourceChecker(),
+            new BCResourceInterfaceChecker(),
+        ));
         $this->debug = (bool) $debug;
     }
 
@@ -49,90 +57,23 @@ class ConfigCache implements ConfigCacheInterface
     {
         @trigger_error('ConfigCache::__toString() is deprecated since version 2.7 and will be removed in 3.0. Use the getPath() method instead.', E_USER_DEPRECATED);
 
-        return $this->file;
-    }
-
-    /**
-     * Gets the cache file path.
-     *
-     * @return string The cache file path
-     */
-    public function getPath()
-    {
-        return $this->file;
+        return $this->getPath();
     }
 
     /**
      * Checks if the cache is still fresh.
      *
-     * This method always returns true when debug is off and the
+     * This implementation always returns true when debug is off and the
      * cache file exists.
      *
      * @return bool true if the cache is fresh, false otherwise
      */
     public function isFresh()
     {
-        if (!is_file($this->file)) {
-            return false;
-        }
-
-        if (!$this->debug) {
+        if (!$this->debug && is_file($this->getPath())) {
             return true;
         }
 
-        $metadata = $this->getMetaFile();
-        if (!is_file($metadata)) {
-            return false;
-        }
-
-        $time = filemtime($this->file);
-        $meta = unserialize(file_get_contents($metadata));
-        foreach ($meta as $resource) {
-            if (!$resource->isFresh($time)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    /**
-     * Writes cache.
-     *
-     * @param string              $content  The content to write in the cache
-     * @param ResourceInterface[] $metadata An array of ResourceInterface instances
-     *
-     * @throws \RuntimeException When cache file can't be written
-     */
-    public function write($content, array $metadata = null)
-    {
-        $mode = 0666;
-        $umask = umask();
-        $filesystem = new Filesystem();
-        $filesystem->dumpFile($this->file, $content, null);
-        try {
-            $filesystem->chmod($this->file, $mode, $umask);
-        } catch (IOException $e) {
-            // discard chmod failure (some filesystem may not support it)
-        }
-
-        if (null !== $metadata && true === $this->debug) {
-            $filesystem->dumpFile($this->getMetaFile(), serialize($metadata), null);
-            try {
-                $filesystem->chmod($this->getMetaFile(), $mode, $umask);
-            } catch (IOException $e) {
-                // discard chmod failure (some filesystem may not support it)
-            }
-        }
-    }
-
-    /**
-     * Gets the meta file path.
-     *
-     * @return string The meta file path
-     */
-    private function getMetaFile()
-    {
-        return $this->file.'.meta';
+        return parent::isFresh();
     }
 }

Resource/BCResourceInterfaceChecker.php
@@ -1,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Config\Resource;
+
+/**
+ * Resource checker for the ResourceInterface. Exists for BC.
+ *
+ * @author Matthias Pigulla <mp@webfactory.de>
+ *
+ * @deprecated since 2.8, to be removed in 3.0.
+ */
+class BCResourceInterfaceChecker extends SelfCheckingResourceChecker
+{
+    public function supports(ResourceInterface $metadata)
+    {
+        /* As all resources must be instanceof ResourceInterface,
+           we support them all. */
+        return true;
+    }
+
+    public function isFresh(ResourceInterface $resource, $timestamp)
+    {
+        @trigger_error('Resource checking through ResourceInterface::isFresh() is deprecated since 2.8 and will be removed in 3.0', E_USER_DEPRECATED);
+
+        return parent::isFresh($resource, $timestamp); // For now, $metadata features the isFresh() method, so off we go (quack quack)
+    }
+}

ResourceCheckerInterface.php
@@ -1,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\Config;
+
+use Symfony\Component\Config\Resource\ResourceInterface;
+
+/**
+ * Interface for ResourceCheckers.
+ *
+ * When a ResourceCheckerConfigCache instance is checked for freshness, all its associated
+ * metadata resources are passed to ResourceCheckers. The ResourceCheckers
+ * can then inspect the resources and decide whether the cache can be considered
+ * fresh or not.
+ *
+ * @author Matthias Pigulla <mp@webfactory.de>
+ * @author Benjamin Klotz <bk@webfactory.de>
+ */
+interface ResourceCheckerInterface
+{
+    /**
+     * Queries the ResourceChecker whether it can validate a given
+     * resource or not.
+     *
+     * @param ResourceInterface $metadata The resource to be checked for freshness
+     *
+     * @return bool True if the ResourceChecker can handle this resource type, false if not
+     */
+    public function supports(ResourceInterface $metadata);
+
+    /**
+     * Validates the resource.
+     *
+     * @param ResourceInterface $resource  The resource to be validated.
+     * @param int               $timestamp The timestamp at which the cache associated with this resource was created.
+     *
+     * @return bool True if the resource has not changed since the given timestamp, false otherwise.
+     */
+    public function isFresh(ResourceInterface $resource, $timestamp);
+
+}